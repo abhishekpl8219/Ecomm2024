f38a9fa079a37739772bf6483f88f61e52e724ed
abhishek <abhishekpl8219@gmail.com>
2024-08-10 12:42:33 +0530

    add login api.

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/authController.js";
 ///router object
 const router = express.Router();
 
 //routing
 //REgister
 router.post("/register", registerController);
-
+//Login || post
+router.post("/login", loginController);
 export default router;
